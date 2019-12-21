f8516548851271d70d43a228e747d70c210e3ba0
Karen <42174311+kawamina@users.noreply.github.com>
2019-12-21 14:53:34 +0100

    After changes

--- oopexe2.py
+++ oopexe2.py
@@ -1,19 +1,15 @@
 
 class Person:
+    def __init__(self, name, nationality, age):
+        self.name = name
+        self.nationality = nationality
+        self.age = age
+
     def introduce_self(self):
         print("My name is "+self.name)
 
-r1 = Person()
-r1.name = 'Tom'
-r1.nationality = 'French'
-r1.age = 30
 
-r1.introduce_self()
+r1 = Person('Tom', 'French', 30)
+r2 = Person('Emma', 'Russian', 56)
 
-r2 = Person()
-r2.name = 'Emma'
-r2.nationality = 'Russian'
-r2.age = 42
-
-r2.introduce_self()
-
+print(r2.introduce_self())
